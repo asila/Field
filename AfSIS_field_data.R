93f8f60760b7c39457eba9b7e8ae525d204234a0
mgwalsh <markusgwalsh@gmail.com>
2015-01-14 03:45:47 +0300

    AfSIS LDSF data summary

--- AfSIS_field_data.R
+++ AfSIS_field_data.R
@@ -1,26 +1,23 @@
-# AfSIS field data summaries
-# M. Walsh, October 2014
-
-# Set your local working directory here e.g.,
-dat_dir <- "/Users/markuswalsh/Documents/LDSF/Field data"
-setwd(dat_dir)
+#' AfSIS field data summaries
+#' M. Walsh, October 2014
 
 # Load packages -----------------------------------------------------------
-
 # install.packages(c("downloader","arm", "wordcloud")), dependencies=TRUE)
 require(downloader)
 require(proj4)
 require(arm)
 require(wordcloud)
 
-# Load data ---------------------------------------------------------------
+#+ Data download -----------------------------------------------------------
+# Create a "Data" folder in your current working directory
+dir.create("AfSIS_Data", showWarnings=F)
+dat_dir <- "./AfSIS_Data"
 
-download("https://www.dropbox.com/s/1oeug9fl2l05tck/AfSIS_field_data.csv.zip?dl=0", "AfSIS_field_data.csv.zip", mode="wb")
-unzip("AfSIS_field_data.csv.zip", overwrite=T)
-fdat <- read.table("AfSIS_field_data.csv", header=T, sep=",")
+# Download AfSIS LDSF data to ./AfSIS_Data"
+download("https://www.dropbox.com/s/kpixjnerwi4d5vx/AfSIS_LDSF_data.csv?dl=0", "./AfSIS_data/AfSIS_LDSF_data.csv", mode="wb")
+fdat <- read.table(paste(dat_dir, "/AfSIS_LDSF_data.csv", sep=""), header=T, sep=",")
 
-# Generate coordinate reference and GID's ---------------------------------
-
+#+ Generate coordinate reference and GID's ---------------------------------
 # Project profile coords to Africa LAEA from LonLat
 fdat.laea <- as.data.frame(project(cbind(fdat$Lon, fdat$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
 colnames(fdat.laea) <- c("x","y")
@@ -35,28 +32,33 @@ gidy <- ifelse(fdat$y<0, paste("S", ygid, sep=""), paste("N", ygid, sep=""))
 GID <- paste(gidx, gidy, sep="-")
 fdat.gid <- cbind(fdat, GID)
 
-# GLMM models -------------------------------------------------------------
-
+#+ GLMM models -------------------------------------------------------------
+# Primarily vegetated areas
 pva.glmer <- glmer(PVA~1+(1|Site), family=binomial(link="logit"), data=fdat.gid)
 pva.coef <- coef(pva.glmer)
 pva.se <- se.coef(pva.glmer)
 
+# Reguarly flooded areas
 wet.glmer <- glmer(WET~1+(1|Site), family=binomial(link="logit"), data=fdat.gid)
 wet.coef <- coef(wet.glmer)
 wet.se <- se.coef(wet.glmer)
 
+# Cultivated & managed areas
 cma.glmer <- glmer(CMA~1+(1|Site), family=binomial(link="logit"), data=fdat.gid)
 cma.coef <- coef(cma.glmer)
 cma.se <- se.coef(cma.glmer)
 
+# Soil depth restrictions to 30 cm
 dr30.glmer <- glmer(cbind(DR30,4-DR30)~1+(1|Site), family=binomial(link="logit"), data=fdat.gid)
 dr30.coef <- coef(dr30.glmer)
 dr30.se <- se.coef(dr30.glmer)
 
+# Visible signs of soil erosion
 vsse.glmer <- glmer(cbind(VSSE,4-VSSE)~1+(1|Site), family=binomial(link="logit"), data=fdat.gid)
 vsse.coef <- coef(vsse.glmer)
 vsse.se <- se.coef(vsse.glmer)
 
+# Woody vegetation cover >60%
 wcs.glmer <- glmer(cbind(WCS,4-WCS)~1+(1|Site), family=binomial(link="logit"), data=fdat.gid)
 wcs.coef <- coef(wcs.glmer)
 wcs.se <- se.coef(wcs.glmer)
@@ -80,7 +82,6 @@ fd$WCSs <- wcs.se$Site[,1]
 write.csv(fd, "Field_data_summary.csv")
 
 # Site comparison ---------------------------------------------------------
-
 # Setup
 fdvars <- c("PVAp","WETp","CMAp","DR30p","VSSEp","WCSp")
 fdmod <- fd[fdvars]
